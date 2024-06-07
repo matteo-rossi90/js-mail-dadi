44080a043624a0d2fc6b2d69aa6a91ba6698bf44
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-07 17:09:51 +0200

    feat: add js code that create an array of available mail list and compare it with user's mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -3,11 +3,34 @@ appropriato sull’esito del controllo */
 
 /////////////////////////////////////////////////////////////////////////
 
+//creare un messaggio
+let message;
+
 //chiedere all'utente la mail
+let inputMail = prompt('Scrivi la tua mail');
+
+console.log(inputMail);
 
 //creare un'array che contiene una serie di mail
 
-//confrontare il dato immesso dall'utente con quelli della lista
+const listMail = ["chicco@gmail.com", "anastasia@gmail.com", "patrizio.bellotto@hotmail.it", 
+"teresa67@hotmail.it", "marialasuperba@gmail.com", "antonio87@yahoo.it"];
 
-    // se la mail corrisponde, comunicare all'utente che può passare
-    //altrimenti non può passare
+console.log(listMail);
+
+//estrarre i nomi delle mail
+
+for(let i = 0; i < listMail.length; i++){
+
+    let elementList = listMail[i];
+
+    //confrontare il dato immesso dall'utente con quelli della lista
+    if (inputMail === elementList) {
+        message = 'La mail che hai inserito corrisponde';// se la mail è valida, comunicare all'utente che è valida
+        console.log(message);
+    } else {
+        message = 'La mail che hai inserito non è tra quelle valide. Riprova';//altrimenti comunicare che la mail non è valida
+        console.log(message);
+    }
+
+}
