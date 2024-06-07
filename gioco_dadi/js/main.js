9db486889e7c3ac642d1f01c32a7ea53ef26c7a0
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-07 15:23:13 +0200

    Creazione codice js - gioco dadi

--- gioco_dadi/js/main.js
+++ gioco_dadi/js/main.js
@@ -4,16 +4,51 @@ in base a chi fa il punteggio più alto */
 
 ///////////////////////////////////////////////////////////////
 
-//genera un numero random per il pc;
+//variabile globale del messaggio da generare
+let message;
 
-//chiedere al giocatore un numero random da 1 a 6
+//genera un numero random per il pc
+let min = 1;
+let max = 7;
+let numPC = Math.floor(Math.random()* (max - min) + min);
 
-//stabilire il vincitore in base al punteggio
+console.log('Il numero generato dal pc è: '+ numPC);
 
-    // se giocatore > PC comunicare al giocatore che ha vinto
+//creare comando che consente al bottone di generare un numero 
+//automatico per il giocatore
 
-    // se giocatore = PC comunicare che non c'è nessun vincitore
+const button = addEventListener('click',
+    function(){
 
-    // altrimenti comunicare che ha perso
+        let numPlayer = parseInt(Math.floor(Math.random() * (max - min) + min));
+        console.log('Il numero generato dal giocatore è: ' + numPlayer);
 
+        //selezionare il contenitore che riceverà il messaggio
+        const content = document.querySelector('.container-box');
+
+        //creare un contenitore in container-box
+        let messageBox = document.createElement('div');
+        content.append(messageBox);
+
+        //stabilire il vincitore in base al punteggio
+
+        if (numPlayer > numPC) {// se giocatore > PC comunicare al giocatore che ha vinto
+
+            message = 'Hai vinto!!!';
+            messageBox.append(message);
+            console.log(message);
+
+        } else if (numPlayer === numPC) {// se giocatore = PC comunicare che non c'è nessun vincitore
+            message = 'Nessun vincitore. E vabbeh, capita!'
+            messageBox.append(message);
+            console.log(message);
+        } else { // altrimenti comunicare che ha perso
+            message = 'Hai perso! Ma non ti vergogni?';
+            messageBox.append(message);
+            console.log(message);
+        }                                                                  
+
+    }
+    
+);
 
