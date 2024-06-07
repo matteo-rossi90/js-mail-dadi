daa3943abaa006f3c8e8a5a80a6ec586e6318e9b
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-07 14:00:41 +0200

    Impostazione iniziale: creazione cartelle e struttura di base

---
+++ gioco_dadi/js/main.js
@@ -0,0 +1,19 @@
+/*Generare un numero random da 1 a 6, sia per il 
+giocatore sia per il computer. Stabilire il vincitore, 
+in base a chi fa il punteggio più alto */
+
+///////////////////////////////////////////////////////////////
+
+//genera un numero random per il pc;
+
+//chiedere al giocatore un numero random da 1 a 6
+
+//stabilire il vincitore in base al punteggio
+
+    // se giocatore > PC comunicare al giocatore che ha vinto
+
+    // se giocatore = PC comunicare che non c'è nessun vincitore
+
+    // altrimenti comunicare che ha perso
+
+
