82348af298d983e42cddde0b0b9c6323dcb78633
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-07 16:25:48 +0200

    build: add base project files and steps in js code - mail list

---
+++ mail/js/main.js
@@ -0,0 +1,13 @@
+/*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio 
+appropriato sull’esito del controllo */
+
+/////////////////////////////////////////////////////////////////////////
+
+//chiedere all'utente la mail
+
+//creare un'array che contiene una serie di mail
+
+//confrontare il dato immesso dall'utente con quelli della lista
+
+    // se la mail corrisponde, comunicare all'utente che può passare
+    //altrimenti non può passare
